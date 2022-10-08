278bced95331d7e8ea64e738c12df9a6645b4891
Alptuğ TÜRKİŞ <carpediem.2323@gmail.com>
2022-10-08 16:49:56 +0000

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
-ReactDOM.render(<p>Hello Webpack </p>, document.getElementById("app"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<p>Hello Webpack </p>, document.getElementById('app'));
